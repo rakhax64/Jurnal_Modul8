3c033182e5e4d4e2cf8978718c3b8b625c922cf1
RakhaPW <rakhapratama@student.telkomuniversity.ac.id>
2022-04-23 14:21:46 +0700

    Menambahkan class Movie

---
+++ modul8_1302194084/modul8_1302194084/Controllers/MovieController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace modul8_1302194084.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MovieController
+    {
+        // GET: api/<MoviesController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // POST api/<MoviesController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // GET api/<MoviesController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // DELETE api/<MoviesController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ modul8_1302194084/modul8_1302194084/Movie.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace modul8_1302194084
+{
+    public class Movies
+    {
+        private string Title { get; set; }
+        private string Director { get; set; }
+        private List<string> Stars { get; set; }
+        private String Description { get; set; }
+    }
+}
